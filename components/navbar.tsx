cade1ba5dc5195c838c810c26e84117aa08ff8b5
kaniishka-sahoo <kanishka.sa.hoo@gmail.com>
2024-08-22 09:14:01 +0530

    io

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <nav className="flex flex-1 border-b border-green-800 w-full h-auto pb-4">
             <a href="/">
-                <h1 className="text-3xl text-green-800 my-auto">Kanishka's Blog</h1>
+                <h1 className="text-3xl text-green-800 my-auto">Kanishka&apos;s Blog</h1>
             </a>
             <a href="/aboutme" className="ml-auto my-auto">
                 <h2 className="text-lg text-green-900 hover:font-semibold">About Me</h2>
